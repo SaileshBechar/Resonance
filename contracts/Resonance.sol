9126be7d6c58403a63c026e95b8e3cac3c091e40
Sailesh Bechar <saileshbechar@yahoo.ca>
2022-10-17 16:58:46 -0700

    added some instructions

--- contracts/Resonance.sol
+++ contracts/Resonance.sol
@@ -24,13 +24,13 @@ contract Resonance is ERC1155, VRFConsumerBaseV2 {
     uint64 public s_subscriptionId;
 
     // Rinkeby coordinator
-    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    address vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
 
     // Rinkeby LINK token contract
-    address link_token_contract = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
+    address link_token_contract = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
 
     // The gas lane to use, which specifies the maximum gas price to bump to
-    bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+    bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
 
     // Depends on the number of requested values that you want sent to the
     // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
